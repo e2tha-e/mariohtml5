3308d8ab29a7122c30f06c8714b43ac37a375f09
electric-el <electric-el@electric-eloquence.io>
2021-10-13 17:11:49 -0400

    adjusted electron window size

--- electron.js
+++ electron.js
@@ -6,7 +6,7 @@ var BrowserWindow = electron.BrowserWindow;
 var win;
 
 app.on('ready', function() {
-  win = new BrowserWindow({width: 656, height: 540});
+  win = new BrowserWindow({width: 656, height: 500});
   win.webContents.session.clearCache(function() {
     win.loadURL(`file://${__dirname}/main.html`);
   });
